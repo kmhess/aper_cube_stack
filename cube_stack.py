5514c7e4ebfe6d300e19cd04879653a82ee94504
Kelley M. Hess <hess@astro.rug.nl>
2022-03-26 15:26:07 +0100

    Implement get_common_spectrum for fields w multiple taskids

--- cube_stack.py
+++ cube_stack.py
@@ -11,7 +11,7 @@ import dask.array as da
 
 import time as testtime
 
-from modules.functions import get_taskids, topo2bary_corr
+from modules.functions import get_taskids, get_common_spectrum
 from modules.telescope_params import westerbork
 
 
@@ -60,96 +60,64 @@ all_nan = True
 if (len(taskids) == len(processed_ids)) or args.force:
     print("\tTASKIDS: {}".format(taskids))
     print("\tPROCESSED TASKIDS: {}".format(processed_ids))
+
     if len(processed_ids) > 1:
-        # Find field center for a given pointing (work even with potentially legacy files):
+        # Find field center for a given pointing (including files where survey prefix may have changed):
         master_list = ascii.read(package_dir + '/data/apertif_v12.21apr06.txt', format='fixed_width')
         pointing, entry = next(([s, m] for s, m in zip(master_list['name'], master_list) if field[1:] in s), None)
+
         # Assign the bary center based on the center of the pointing itself (always beam 0 saved in files)
         barycent_pos = SkyCoord(ra=entry['ra'], dec=entry['dec'], unit='deg')
-        time = [None] * len(processed_ids)
-        data_all = []
-        for b in beams:
-            # Calculate barycentric shifts for each cube
-            delta_chan = []
-            new_crval3 = []
-            for ii, t in enumerate(taskids):
-                # Get info from the header
-                filename = str(t) + '/B0' + str(b).zfill(2) + '/HI_image_cube' + str(c) + '.fits'
-                try:
-                    os.system('cp {} {}_test.fits'.format(filename, filename[:-5]))
-                    hdu = fits.open(filename)
-                    # hdu = fits.open(filename[:-5] + '_test.fits', mode=update)
-                    print("\tFound cube {} for {} beam {:02}".format(c, t, b))
-                    all_nan = False
-                except FileNotFoundError:
-                    print('\t{} has no cube {} for beam {:02}'.format(t, c, b))
-                    new_crval3.append(np.nan)
-                    delta_chan.append(np.nan)
-                    continue
-                header = hdu[0].header
-                if time[ii] == None:
-                    time[ii] = Time(header['DATE-OBS'])
-                if cdelt3 == 0.0:
-                    cdelt3 = header['CDELT3']
-                # Build in a check if the header is already in BARYCENT!
-                if hdu[0].header['SPECSYS'] == 'BARYCENT':
-                    print("\tCube already in barycentric reference frame, continuing.")
-                    delta_crval3 = 0 * u.Hz
-                else:
-                    print("\tAssuming cube is in topecentric reference frame; transforming to barycentric")
-                    # Calculate barycentric correction
-                    spec_coord = SpectralCoord(header['CRVAL3'], unit='Hz',
-                                               observer=westerbork().get_itrs(obstime=time[ii]),
-                                               target=barycent_pos)
-                    bary_spec_coord = spec_coord.with_observer_stationary_relative_to('icrs')
-                    delta_crval3 = spec_coord - bary_spec_coord
-                    # hdu[0].header['CRVAL3'] = bary_spec_coord.value
-                    # hdu[0].header['SPECSYS'] = 'BARYCENT'
-                    # hdu[0].header['CTYPE3'] = 'FREQ'
-                    # hdu.flush()
 
-                hdu.close()
-                # Calculate int(channel shift)
-                new_crval3.append(bary_spec_coord.value)
-                delta_chan.append(np.round(np.array(delta_crval3.value) / cdelt3))
+        # Find common spectrum across all beams for a field.
+        delta_chan, new_crval3, naxis2, naxis3 = get_common_spectrum(barycent_pos, processed_ids, c)
+
+        for b in beams:
+            # Create subcubes
+            n_chans = int(naxis3 - np.abs(np.nanmin(delta_chan)-np.nanmax(delta_chan)))
+            skip_chans = delta_chan - np.nanmin(delta_chan)
+            nan_cube = np.empty((int(n_chans), naxis2, naxis2)) * np.nan
+
+            # Start counter
+            tic = testtime.perf_counter()
+            data_all = []
+            rms = []
+            all_nan = True
+            # Calculate noise, weight for each cube
+            for j, t in zip(skip_chans, taskids):
+                try:
+                    filename = str(t) + '/B0' + str(b).zfill(2) + '/HI_image_cube' + str(c) + '.fits'
+                    data_all.append(fits.getdata(filename)[int(j):int(j+n_chans), :, :])
+                    rms.append(da.nanstd(da.array(data_all)[-1, :, :, :], axis=(1, 2)).compute())
+                    # Write noise, weight as a function of channel to a text file for each cube.
+                    ascii.write([list(range(int(j), int(j) + n_chans)), np.array(rms)[-1, :]],
+                                str(t) + '/B0' + str(b).zfill(2) + '/noise.txt',
+                                names=['chan', 'noise'], overwrite=True)
+                    all_nan = False
+                    last_file = filename
+                except FileNotFoundError:
+                    # Fill with nan values if the beam cube doesn't exist.
+                    data_all.append(nan_cube)
+                    rms.append(da.std(da.array(data_all)[-1, :, :, :], axis=(1, 2)).compute())
 
             # Skip beam if there is no contributing data from any taskids
             if all_nan == True:
                 print("\tNo data for beam {:02} in any taskid covering field {}.".format(b, field))
                 continue
 
-            # Create subcubes
-            n_chans = int(header['NAXIS3'] - np.abs(np.nanmin(delta_chan)-np.nanmax(delta_chan)))
-            skip_chans = delta_chan - np.nanmin(delta_chan)
-            nan_cube = np.empty((int(n_chans), header['NAXIS2'], header['NAXIS2'])) * np.nan
-
-            tic = testtime.perf_counter()
-            data_all = []
-            rms = []
-            # Calculate noise, weight for each cube
-            for j, t in zip(skip_chans, taskids):
-                try:
-                    data_all.append(fits.getdata(str(t) + '/B0' + str(b).zfill(2) + '/HI_image_cube' + str(c) +
-                                                 '.fits')[int(j):int(j+n_chans), :, :])
-                    rms.append(da.nanstd(da.array(data_all)[-1, :, :, :], axis=(1, 2)).compute())
-                    # Write noise, weight as a function of channel to a text file for each cube.
-                    ascii.write([list(range(int(j), int(j) + n_chans)), np.array(rms)[-1, :]],
-                                str(t) + '/B0' + str(b).zfill(2) + '/noise.txt',
-                                names=['chan', 'noise'], overwrite=True)
-                except FileNotFoundError:
-                    # Fill with nan values if the beam cube doesn't exist.
-                    data_all.append(nan_cube)
-                    rms.append(da.std(da.array(data_all)[-1, :, :, :], axis=(1, 2)).compute())
-
+            # Get a template header
+            header = fits.getheader(last_file)
+            # Prepare data arrays
             data_all = np.array(data_all)
             rms = np.array(rms)
             weights = 1/rms**2
 
-            # Weight cube, combine, and normalize
+            # Weight cube, combine, and normalize data cubes
             dataweight = data_all.transpose()*weights.transpose()
             num = da.nansum(dataweight.transpose(), axis=0)
             denom = da.nansum(weights, axis=0)
             combo_cube = (num.transpose()/denom).compute().transpose()
+            # End counter
             toc = testtime.perf_counter()
             print(f"Do median: {toc - tic:0.4f} seconds")
 
@@ -169,11 +137,15 @@ if (len(taskids) == len(processed_ids)) or args.force:
             print(f"Do write: {toc1 - tic1:0.4f} seconds")
 
     elif len(processed_ids) == 1:
+
         print("\tOnly one taskid.  Doing barycent correction & writing to a folder with the field naming scheme.")
         # Find field center for a given pointing (work even with potentially legacy files):
         master_list = ascii.read(package_dir + '/data/apertif_v12.21apr06.txt', format='fixed_width')
         pointing, entry = next(([s, m] for s, m in zip(master_list['name'], master_list) if field[1:] in s), None)
+
+        # Assign the bary center based on the center of the pointing itself (always beam 0 saved in files)
         barycent_pos = SkyCoord(ra=entry['ra'], dec=entry['dec'], unit='deg')
+
         for b in beams:
             # Get info from the header
             filename = str(processed_ids[0]) + '/B0' + str(b).zfill(2) + '/HI_image_cube' + str(c) + '.fits'
@@ -181,7 +153,7 @@ if (len(taskids) == len(processed_ids)) or args.force:
                 hdu = fits.open(filename)
                 print("\tFound cube {} for {} beam {:02}".format(c, t, b))
             except FileNotFoundError:
-                print('\t{} has no cube {} for beam {:02}'.format(t, c, b))
+                print("\tNo data for beam {:02} in taskid {} covering field {}.".format(b, t, field))
                 continue
             header = hdu[0].header
             if b == beams[0]:
